71a0a60c20abcd12b7d791c4748800f59bb410e1
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-24 16:20:51 -0800

    Added form to search for student

--- hackbright-web.py
+++ hackbright-web.py
@@ -16,6 +16,13 @@ def get_student():
                            last=last,
                            github=github)
 
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student"""
+
+    return render_template("student_search.html")
+
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
     app.run(debug=True)
